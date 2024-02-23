f1c087b607f70b0af6a8e55bfe7fa3cd5ab5192d
Adam <adam.p.clarke@cranfield.ac.uk>
2024-02-23 11:10:38 +0000

    run_cases.py

--- src/2_run_cases.py
+++ src/2_run_cases.py
@@ -1,1 +1,20 @@
-# Code to run all of the generated cases
+import os
+from pathlib import Path
+import subprocess
+# import multiprocessing as mp
+
+def main():
+    path = Path("cases")
+    cases = os.listdir(path)
+    cpus = os.cpu_count()
+
+    for case in cases:
+        subprocess.run([path/case/"Allrun"])
+        break
+
+    # run_cases = [[path/case/"Allrun"] for case in cases]
+    # with mp.Pool(processes=cpus) as p:
+    #     p.map(subprocess.run, run_cases)
+
+if __name__ == "__main__":
+    main()
