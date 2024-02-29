20d61ebb1bf88eaa53bf9da3335db90a06c67074
Adam <adam.p.clarke@cranfield.ac.uk>
2024-02-29 15:57:02 +0000

    Add argument for samples to run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+samples=$1
+
 Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
 sleep 3
 
-## python3 src/1_generate_cases.py
+## python3 src/1_generate_cases.py $samples
 ## python3 src/2_run_cases.py
 ## python3 src/3_postprocess_cfd_data.py
